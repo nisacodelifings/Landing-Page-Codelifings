43dc0c6f4dddd3da26a4851c73d5eb487a40d800
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-09-29 15:57:04 +0700

    mantap

--- src/Content2.js
+++ src/Content2.js
@@ -7,26 +7,25 @@ export default function Content2() {
         <div className="judul">
           <h3>Karya Codelifings</h3>
         </div>
-        <div className="isi"  style={{textAlign:"center",color:"#C8578F"}}>
-          <img style={{borderRadius:"50%",width:"100px"}} src="https://res.cloudinary.com/codelifings/image/upload/v1584323346/images_qpkdkz.jpg" />
+        <div className="isi" style={{ textAlign: "center", color: "#C8578F" }}>
+          <img
+            style={{ borderRadius: "50%", width: "100px" }}
+            src="https://res.cloudinary.com/codelifings/image/upload/v1584323346/images_qpkdkz.jpg"
+          />
           <br />
           <br />
-          by Owner <a
-            style={{ color: "white" }}
-            className="nisa"
-            href="https://nisacodelifings.netlify.com"
-          >
-            Annisa Puspitasari
-          </a>
-         <br /><br />
-        <img src="https://res.cloudinary.com/codelifings/image/upload/v1598089193/Screenshot_59_kvqzzm.png" />
+
+          <img src="https://res.cloudinary.com/codelifings/image/upload/v1598089193/Screenshot_59_kvqzzm.png" />
           <img src="https://res.cloudinary.com/codelifings/image/upload/v1595574549/screenshot_-2020-04-02-simpleazy-1_2_herfoy.png" />
           <img src="https://res.cloudinary.com/codelifings/image/upload/v1585726690/a_zftq8g.png" />
-           <p>
-           Salah satu Client yg menggunakan domain sendiri (.online){" "}
-            <a href="https://agungjayakreasi.online">Ajkprinting</a>.
+          <p>
+            Salah satu Client yg menggunakan domain sendiri (.online){" "}
+            <a href="https://agungjayakreasi.online">Ajkprinting</a>. dan masih
+            banyak lagi anda bisa cek{" "}
+            <a href="https://nisacodelifings.netlify.com/codelifings.html">
+              disini
+            </a>
           </p>
-
         </div>
       </div>
     </main>
